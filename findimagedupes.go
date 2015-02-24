a22b42b336f74adfba8ae6d59ac1afc894fdf846
opennota <opennota@gmail.com>
2015-02-25 00:21:11 +0700

    Add package description

--- findimagedupes.go
+++ findimagedupes.go
@@ -11,6 +11,7 @@
 // You should have received a copy of the GNU General Public License along
 // with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+// Find visually similar or duplicate images.
 package main
 
 import (
